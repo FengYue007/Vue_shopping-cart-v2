e40b582cc86e498f59553960d55eb76fe30753d1
FengYue <10761101@gm.nfu.edu.tw>
2021-08-19 20:22:22 +0800

    購物車 component 完成

--- vite-cart/src/App.vue
+++ vite-cart/src/App.vue
@@ -19,134 +19,62 @@
     </header>
 
     <section class="container mt-4">
-      <div class="items row">
-
-        <div class="col-sm-2" v-for="(item, idx) in itemList">
-          <div class="card" :data-product-id="item.id">
-            <img :src="`images/${item.cover}`" src="images/cat001.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">{{ idx + 1 }} {{ item.name }}</h5>
-              <p class="price">${{ item.price }}</p>
-              <button
-                @click="addCart(item)"
-                class="btn btn-sm btn-warning fw-light">
-                <i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <ItemList :itemList="itemList" @add="addToCart" />
       <hr>
 
-      // 查看購物車狀態
-      <!-- <pre> {{ cart }} </pre> -->
-      <!-- <pre> {{ totalPrice }} </pre> -->
-
-      <section class="cart">
-        <h2>購物車</h2>
-        <table class="table cart-item-table">
-          <thead>
-            <tr>
-              <th scope="col">項目</th>
-              <th scope="col">數量</th>
-              <th scope="col">單價</th>
-              <th scope="col">小計</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in cart">
-              <td>{{ item.name }}</td>
-              <td><input type="number" class="quantity" min="1" v-model="item.qty"></td>
-              <td>${{ item.price }}</td>
-              <td>${{ item.price * item.qty }}</td>
-              <td>
-                <button
-                  @click="removeCart(item.id)"
-                  class="remove-item-btn btn btn-danger btn-sm">
-                  <i class="fas fa-trash-alt"></i></button>
-              </td>
-          </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colspan="2"></td>
-              <td>總價</td>
-              <td><span class="total-price">${{ totalPrice }}</span></td>
-              <td></td>
-            </tr>
-          </tfoot>
-        </table>
-        <button
-          @click="removeAllItem"
-          class="btn btn-lg btn-success empty-cart">
-          <i class="fas fa-baby-carriage"></i> 清空購物車</button>
-      </section>
+      <CartList :cartList="cartList"
+        @remove="removeFromCart"
+        @remove-all="cleanCart"
+        />
     </section>
   </main>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
-
+import { ref } from 'vue';
+import ItemList from './components/item.vue';
+import CartList from './components/cart.vue';
 export default {
+  components: {
+    ItemList,
+    CartList
+  },
   setup() {
+    // 商品列表
     const itemList = ref([]);
     fetch('./item.json')
-      .then( res => res.json())
-      // .then( d => itemList.value = itemList.value.concat(d) );
-      .then( d => itemList.value = d );
+      .then(res => res.json())
+      .then(d => itemList.value = itemList.value.concat(d));
+    // 購物車列表
+    const cartList = ref([]);
 
-
-      const cart = ref([]);
-
-      const addCart = item => {
-        const idx = cart.value.findIndex(d => item.id === d.id)
-
-        if( idx > -1 ) {
-          cart.value[idx].qty++;
-        }
-        else {
-          cart.value.push({
-            ...item,
-            qty: 1
-          });
-        }; 
-      };
-
-
-      const totalPrice = computed(() => {
-        return (
-          Math.round(
-            cart.value.reduce(
-              (total, currentitem) => total + currentitem.price * currentitem.qty,
-              0
-            ) * 100
-          ) / 100
-        );
-      });
-
-      const removeCart = itemId => {
-        cart.value = cart.value.filter(item => item.id !=== itemId)
-      };
-
-      const removeAllItem = () => {
-        cart.value.length= 0;
-      };
-
-
+    // 加入購物車
+    const addToCart = (item) => {
+      const idx = cartList.value.findIndex((d) => item.id === d.id);
+      if (idx > -1) {
+        cartList.value[idx].qty++;
+      } else {
+        cartList.value.push({
+          ...item,
+          qty: 1,
+        });
+      }
+    };
+    // 從購物車移除
+    const removeFromCart = (itemId) => {
+      cartList.value = cartList.value.filter((item) => item.id != itemId);
+    };
+    // 清除購物車
+    const cleanCart = () => {
+      cartList.value.length = 0
+    };
     return {
       itemList,
-      cart,
-      addCart,
-      totalPrice,
-      removeCart,
-      removeAllItem
+      cartList,
+      addToCart,
+      removeFromCart,
+      cleanCart,
     }
   }
 }
 </script>
-
-<style>
-
-</style>
