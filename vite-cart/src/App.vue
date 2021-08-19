28823b6b3c3b912458bd7382ec0b4e6016913a1f
FengYue <10761101@gm.nfu.edu.tw>
2021-08-19 18:15:32 +0800

    購物車 完成

--- vite-cart/src/App.vue
+++ vite-cart/src/App.vue
@@ -20,68 +20,27 @@
 
     <section class="container mt-4">
       <div class="items row">
-        <div class="col-sm-2">
-          <div class="card" data-product-id="624946E">
-            <img src="images/cat001.jpg" class="card-img-top" alt="">
+
+        <div class="col-sm-2" v-for="(item, idx) in itemList">
+          <div class="card" :data-product-id="item.id">
+            <img :src="`images/${item.cover}`" src="images/cat001.jpg" class="card-img-top" alt="">
             <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">老大</h5>
-              <p class="price">$20</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
+              <h5 class="card-title fs-6 fw-light">{{ idx + 1 }} {{ item.name }}</h5>
+              <p class="price">${{ item.price }}</p>
+              <button
+                @click="addCart(item)"
+                class="btn btn-sm btn-warning fw-light">
+                <i class="fas fa-cat"></i></button>
             </div>
           </div>
         </div>
-        <div class="col-sm-2">
-          <div class="card" data-product-id="044464F">
-            <img src="images/cat002.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">貝貝</h5>
-              <p class="price">$15</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="card" data-product-id="A6245B4">
-            <img src="images/cat003.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">老虎</h5>
-              <p class="price">$10</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="card" data-product-id="09DC594">
-            <img src="images/cat004.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">胖胖</h5>
-              <p class="price">$8.5</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="card" data-product-id="AAE759E">
-            <img src="images/cat005.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">小花</h5>
-              <p class="price">$9.99</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="card" data-product-id="D014739">
-            <img src="images/cat006.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title fs-6 fw-light">黑臉</h5>
-              <p class="price">$12.5</p>
-              <button class="btn btn-sm btn-warning fw-light"><i class="fas fa-cat"></i></button>
-            </div>
-          </div>
-        </div>
+
       </div>
       <hr>
+
+      // 查看購物車狀態
+      <!-- <pre> {{ cart }} </pre> -->
+      <!-- <pre> {{ totalPrice }} </pre> -->
 
       <section class="cart">
         <h2>購物車</h2>
@@ -96,24 +55,96 @@
             </tr>
           </thead>
           <tbody>
+            <tr v-for="item in cart">
+              <td>{{ item.name }}</td>
+              <td><input type="number" class="quantity" min="1" v-model="item.qty"></td>
+              <td>${{ item.price }}</td>
+              <td>${{ item.price * item.qty }}</td>
+              <td>
+                <button
+                  @click="removeCart(item.id)"
+                  class="remove-item-btn btn btn-danger btn-sm">
+                  <i class="fas fa-trash-alt"></i></button>
+              </td>
+          </tr>
           </tbody>
           <tfoot>
             <tr>
               <td colspan="2"></td>
               <td>總價</td>
-              <td><span class="total-price">$0</span></td>
+              <td><span class="total-price">${{ totalPrice }}</span></td>
               <td></td>
             </tr>
           </tfoot>
         </table>
-        <button class="btn btn-lg btn-success empty-cart"><i class="fas fa-baby-carriage"></i> 清空購物車</button>
+        <button
+          @click="removeAllItem"
+          class="btn btn-lg btn-success empty-cart">
+          <i class="fas fa-baby-carriage"></i> 清空購物車</button>
       </section>
     </section>
   </main>
 </template>
 
 <script>
+import { computed, ref } from 'vue';
 
+export default {
+  setup() {
+    const itemList = ref([]);
+    fetch('./item.json')
+      .then( res => res.json())
+      // .then( d => itemList.value = itemList.value.concat(d) );
+      .then( d => itemList.value = d );
+
+
+      const cart = ref([]);
+
+      const addCart = item => {
+        const idx = cart.value.findIndex(d => item.id === d.id)
+
+        if( idx > -1 ) {
+          cart.value[idx].qty++;
+        }
+        else {
+          cart.value.push({
+            ...item,
+            qty: 1
+          });
+        }; 
+      };
+
+
+      const totalPrice = computed(() => {
+        return (
+          Math.round(
+            cart.value.reduce(
+              (total, currentitem) => total + currentitem.price * currentitem.qty,
+              0
+            ) * 100
+          ) / 100
+        );
+      });
+
+      const removeCart = itemId => {
+        cart.value = cart.value.filter(item => item.id !=== itemId)
+      };
+
+      const removeAllItem = () => {
+        cart.value.length= 0;
+      };
+
+
+    return {
+      itemList,
+      cart,
+      addCart,
+      totalPrice,
+      removeCart,
+      removeAllItem
+    }
+  }
+}
 </script>
 
 <style>
